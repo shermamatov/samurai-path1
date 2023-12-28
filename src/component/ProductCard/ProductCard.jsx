98380ab9ce6f312cf7e0ec086520fb3a269e6b2b
unknown <timurshermamatov1@gmail.com>
2023-12-28 23:42:35 +0600

    front

--- src/component/ProductCard/ProductCard.jsx
+++ src/component/ProductCard/ProductCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const ProductCard = ({ item }) => {
@@ -11,24 +11,48 @@ const ProductCard = ({ item }) => {
             <div className="relative">
                 <img
                     className="rounded-lg aspect-square object-cover"
-                    src={item.img}
+                    src={item.mainImg}
                     alt=""
                 />
-                <div
-                    className={`absolute flex justify-center items-center rounded-lg top-1 right-1 mob:top-2 mob:right-2 w-[40%] h-4 mob:h-6 ${
-                        item.have ? "bg-sky-500" : "bg-red-500"
-                    }`}
-                >
-                    <span className="lg:text-sm mob:text-xs text-[8px] ">
-                        {item.have ? "в наличии" : "нет в наличии"}
-                    </span>
+                <div className="absolute top-2 left-2 right-2 flex justify-between">
+                    <button
+                        className={`rounded-lg w-[40%] h-4 mob:h-6 ${
+                            item.have ? "bg-sky-500" : "bg-red-500"
+                        }`}
+                    >
+                        <span className="lg:text-sm mob:text-xs text-[8px] ">
+                            {item.have ? "в наличии" : "нет в наличии"}
+                        </span>
+                    </button>
+                    {item.discount > 0 && (
+                        <button
+                            className={`rounded-md w-[40%] h-4 mob:h-6 bg-green-700`}
+                        >
+                            <span className="lg:text-sm mob:text-xs text-[8px] ">
+                                скидка!
+                            </span>
+                        </button>
+                    )}
                 </div>
             </div>
             <h3 className="text-lg mob:text-xl mt-2 mob:mt-3">{item.title}</h3>
             <p className="text-[10px] mob:text-xs mt-1 mob:mt-2 mb-1">
                 {item.desc}
             </p>
-            <strong className="text-sky-500">{item.price} сом</strong>
+            {item.discount > 0 ? (
+                <div className="flex">
+                    <p className="text-lg font-semibold text-sky-500 ">
+                        {item.discount} сом
+                    </p>
+                    <p className="text-lg text-gray-400 line-through ml-2">
+                        {item.price} сом
+                    </p>
+                </div>
+            ) : (
+                <strong className="text-lg font-semibold text-sky-500">
+                    {item.price} сом
+                </strong>
+            )}
         </div>
     );
 };
